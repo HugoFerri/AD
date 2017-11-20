a5679b705f05268a512987a47b6c8120271e4236
HugoFerri <hugoferri94@gmail.com>
2017-11-20 12:48:30 +0100

    Creacion de un comboBox mostrando la informacion de la BD

---
+++ CComboBox/CComboBox/CComboBox/ComboBoxHelper.cs
@@ -0,0 +1,32 @@
+﻿using Gtk;
+using System;
+using System.Data;
+
+namespace SerpisAd.Ad
+{
+    public class ComboBoxHelper
+    {
+        public const String NullLabel = "< Sin asignar >";
+        public static void Fill(ComboBox comboBox, String selectSql, object id){
+            CellRendererText cellRendererText = new CellRendererText();
+
+            comboBox.PackStart(cellRendererText, false);
+            comboBox.AddAttribute(cellRendererText, "text", 1);
+
+			IDbCommand dbCommnand = App.Instance.Connection.CreateCommand();
+			dbCommnand.CommandText = selectSql;
+			IDataReader dataReader = dbCommnand.ExecuteReader();
+
+            ListStore listStore = new ListStore(typeof(String), typeof(String));
+            comboBox.Model = listStore;
+            TreeIter initialTreeIter = listStore.AppendValues("0", NullLabel);
+
+            while(dataReader.Read()){
+                TreeIter treeIter = listStore.AppendValues(dataReader[0].ToString(), dataReader[1].ToString());
+                if (id.Equals(dataReader[0]))
+                    initialTreeIter = treeIter;
+            }
+            comboBox.SetActiveIter(initialTreeIter);
+        }
+    }
+}
---
+++ CComboBox/CComboBox/CComboBox/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+using SerpisAd.Ad;
+using MySql.Data.MySqlClient;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        App.Instance.Connection = new MySqlConnection("server=localhost;database=dbprueba;user=root;password=sistemas");
+        App.Instance.Connection.Open();
+
+        ComboBoxHelper.Fill(comboBox, "SELECT id, nombre FROM categoria ORDER BY nombre", 4UL);
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}

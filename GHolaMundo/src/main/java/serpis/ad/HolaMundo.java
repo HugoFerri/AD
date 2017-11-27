a1c3845356028465f63e8e4956f5ea4b9811650e
HugoFerri <hugoferri94@gmail.com>
2017-11-27 13:29:12 +0100

    Creados los dos primeros proyectos de Gradle y conexiones con Java

---
+++ GHolaMundo/src/main/java/serpis/ad/HolaMundo.java
@@ -0,0 +1,9 @@
+package serpis.ad;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.print("Hola mundo desde Java con Eclipse usando Gradle");
+	}
+
+}
---
+++ GMySql/src/main/java/serpis/ad/PruebaMySql.java
@@ -0,0 +1,36 @@
+package serpis.ad;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PruebaMySql {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		String DB_URL = "jdbc:mysql://localhost/dbprueba";
+		String USER = "root";
+		String PASS = "sistemas";
+		String sql;
+		   
+		Connection connection = DriverManager.getConnection(DB_URL,USER,PASS);
+		Statement stmt = connection.createStatement();
+
+	    Class.forName("com.mysql.cj.jdbc.Driver");
+
+	    sql = "SELECT * FROM categoria";
+	    ResultSet rs = stmt.executeQuery(sql);
+	    
+	    while(rs.next()){
+	         int id  = rs.getInt("id");
+	         String nombre = rs.getString("nombre");
+
+	         System.out.println("ID: " + id + " nombre: " + nombre);
+	      }
+	    
+	      rs.close();
+	      stmt.close();
+	      connection.close();
+	}
+}

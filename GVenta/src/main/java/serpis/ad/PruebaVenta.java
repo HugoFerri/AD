aff16fe1981ef73b06989b6b08714da691d821fc
HugoFerri <hugoferri94@gmail.com>
2018-01-31 12:40:52 +0100

    Añadidos los metodos newCategoria y newCliente funcionando, newPedido en construccion

--- GVenta/src/main/java/serpis/ad/PruebaVenta.java
+++ GVenta/src/main/java/serpis/ad/PruebaVenta.java
@@ -24,13 +24,16 @@ public class PruebaVenta {
         while (!salir) {
  
         	System.out.println("0. Salir");
-            System.out.println("1. Mostrar todo");
+            System.out.println("1. No hace nada");
             System.out.println("2. Mostrar articulos");
             System.out.println("3. Mostrar categoria");
             System.out.println("4. Mostrar clientes");
             System.out.println("5. Mostrar pedidos");
             System.out.println("6. Mostrar los pedidos de las lineas");
             System.out.println("7. Añadir articulo");
+            System.out.println("8. Añadir categoria");
+            System.out.println("9. Añadir cliente");
+            System.out.println("10. Añadir pedido");
  
             try {
  
@@ -42,7 +45,7 @@ public class PruebaVenta {
                 		salir = true;
                 		break;     
                 	case 1:
-                		showAll(Articulo.class);
+                		
                         break;
                     case 2:
                     	showAll(Articulo.class);
@@ -61,6 +64,15 @@ public class PruebaVenta {
                         break;
                     case 7:
                     	newArticulo();
+                        break;
+                    case 8:
+                    	newCategoria();
+                        break;
+                    case 9:
+                    	newCliente();
+                        break;
+                    case 10:
+                    	newPedido();
                         break;
                     default:
                         System.out.println("No ha introducido un numero del menú");
@@ -89,14 +101,51 @@ public class PruebaVenta {
 	private static void newArticulo() {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
-		Categoria categoria = entityManager.getReference(Categoria.class, 1);
-		
+		Categoria categoria = entityManager.getReference(Categoria.class, 1L);
 		Articulo articulo = new Articulo();
-		articulo.setNombre("nuevo" + new Date());
-		articulo.setPrecio(new BigDecimal (6));
+		articulo.setNombre("nuevo " + new Date());
+		articulo.setPrecio(new BigDecimal(6));
 		articulo.setCategoria(categoria);
 		entityManager.persist(articulo);
 		entityManager.getTransaction().commit();
+	}
+	
+	private static void newCategoria() {
+		Categoria categoria = new Categoria();
+		categoria.setNombre("Categoria " + Math.floor(Math.random()*10+1));
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(categoria);
+		entityManager.getTransaction().commit();
+	}
+	
+	public static void newCliente(){
+		Cliente Cliente = new Cliente();
+		Cliente.setNombre("Cliente " + Math.floor(Math.random()*10+1));
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(Cliente);
+		entityManager.getTransaction().commit();
+	}
+	
+	public static void newPedido() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		Pedido pedido = new Pedido();
+		Cliente cliente = entityManager.getReference(Cliente.class, 1L);
+		pedido.setCliente(cliente);
+		PedidoLinea pedidoLinea1 = new PedidoLinea();
+		pedido.add(pedidoLinea1);
+//		//0j0 las dos sentencias siguientes mantienen sincronizada la asociación
+//		pedido.getPedidoLineas().add(pedidoLinea1);
+//		pedidoLinea1.setPedido(pedido);
+		Articulo articulo = entityManager.getReference(Articulo.class, 1L);
+		pedidoLinea1.setArticulo(articulo);
 		
+		entityManager.persist(pedido);
+		entityManager.getTransaction().commit();
+		
+		for(PedidoLinea pedidoLinea : pedido.getPedidoLineas())
+			System.out.println(pedidoLinea);
 	}
 }
